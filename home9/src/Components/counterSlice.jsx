df190c8f3082ed3994fb1f08c14af32a136aec89
Yarikdolbiev <raisindolbiev@gmail.com>
2024-01-22 08:53:30 +0200

    123

--- home9/src/Components/counterSlice.jsx
+++ home9/src/Components/counterSlice.jsx
@@ -1,56 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const incrementCallback = (state) => {
-  state.value += 1;
-};
-
-const decrementCallback = (state) => {
-  state.value = Math.max(0, state.value - 1);
-};
-
-const createCounterSlice = () => {
-  return createSlice({
-    name: "counter",
-    initialState: {
-      value: 0,
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
     },
-    reducers: {
-      increment: incrementCallback,
-      decrement: decrementCallback,
+    decrement: (state) => {
+      state.value = Math.max(0, state.value - 1);
     },
-  });
-};
+  },
+});
 
-export { incrementCallback, decrementCallback };
-
-const counterSlice = createCounterSlice();
-export const selectCounter = state => state.counter.value;
 export const { increment, decrement } = counterSlice.actions;
+
+export const selectCounter = (state) => state.counter.value;
+
 export default counterSlice.reducer;
-
-
-
-
-
-// import { createSlice } from '@reduxjs/toolkit';
-
-// export const counterSlice = createSlice({
-//   name: 'counter',
-//   initialState: {
-//     value: 0,
-//   },
-//   reducers: {
-//     increment: state => {
-//       state.value += 1;
-//     },
-//     decrement: state => {
-//       state.value = Math.max(0, state.value - 1);
-//     },
-//   },
-// });
-
-// export const { increment, decrement } = counterSlice.actions;
-
-// export const selectCounter = state => state.counter.value;
-
-// export default counterSlice.reducer;
